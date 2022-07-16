c068ca4ed2b0c4950e7ddb16ea6147a16bc9f4a5
Tom <Tom@qq.com>
2022-07-16 13:45:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author snkstart
+ * @create 2022-07-16 13:41
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
